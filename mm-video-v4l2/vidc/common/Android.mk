88390ea5bcafc53970625ab870e152ec756b49bf
Mohd Faraz <mohd.faraz.abc@gmail.com>
2020-03-24 06:59:33 +0530

    media: generalise its path
     * Since QCOM_MEDIA_ROOT is the definition of our hal path, so when can simply use it where ever hal is cloned
    
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -25,8 +25,8 @@ endif
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-ibmm-vidc-inc      += $(TOP)/hardware/qcom/media/mm-core/inc
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/$(QCOM_MEDIA_ROOT)/mm-core/inc
+libmm-vidc-inc      += $(TOP)/$(QCOM_MEDIA_ROOT)/libc2dcolorconvert
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 libmm-vidc-inc      += $(LIBION_HEADER_PATHS)
 
